76b600328b616008fd42cdbef74cfa92a428247e
saraarch <saranarchi.1@gmail.com>
2023-04-03 12:30:21 +0530

    https://github.com/saraarch/netlify-deploy.git

---
+++ script.js
@@ -0,0 +1,7 @@
+var request = new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v2/all");
+request.send();
+request.onload=function(){
+    var result=JSON.parse(request.response);
+    console.log(result);
+}
